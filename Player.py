5bd63ab6488e7031a3e8c67ae9c23360a3ac4807
Max Goldstein <maxgoldstein1@gmail.com>
2012-03-12 17:39:47 -0400

    Player sprite class; control handling in game.py

---
+++ Player.py
@@ -0,0 +1,57 @@
+import pygame
+from pygame.locals import *
+
+def bound (lo, val, hi):
+    return max(lo, min(val, hi))
+
+class Player(pygame.sprite.Sprite):
+    def __init__ (self, screen, x, y):
+        self.screen = screen
+        self.x = x
+        self.y = y
+        
+        self.dx = 0
+        self.dy = 0
+
+        #Gravitational acceleration
+        self.g = -2
+
+        #Walking speed
+        self.walkSpeed = 10
+
+        #Boundaries
+        self.maxx = self.screen.get_width()
+        self.maxy = self.screen.get_height()
+        #minimums assumed to be zero
+
+    def update(self, commands, gravity):
+        self.gravity = gravity
+        
+        if gravity:
+            #todo: collision detection for standing on platforms
+            #currently always in freefall
+            self.dy -= self.g
+        else:
+            self.dx = 0
+            self.dy = 0
+            #Zero out velocities, then add or subtract = simplest way to deal
+            #with holding down both left and right keys
+            if K_RIGHT in commands:
+                self.dx += self.walkSpeed
+            if K_LEFT in commands:
+                self.dx -= self.walkSpeed
+            if K_UP in commands:
+                self.dy -= self.walkSpeed
+            if K_DOWN in commands:
+                self.dy += self.walkSpeed
+        
+        self.x += self.dx
+        self.y += self.dy
+        
+        #Don't go off edge of screen
+        self.x = bound(0, self.x, self.maxx)
+        self.y = bound(0, self.y, self.maxy)
+
+    def draw(self):
+        #Dummy draw method
+        pygame.draw.circle(self.screen, (0, 0, 0), (self.x, self.y), 5)
--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import pygame, sys, os
 from pygame.locals import *
+from Player import Player
 
 def quit():
     pygame.quit()
@@ -15,6 +16,17 @@ clock = pygame.time.Clock()
 FPS = 50
 time_passed = 0
 
+#static list of controls
+controls = [K_UP, K_DOWN, K_LEFT, K_RIGHT]
+#dynamic list of active commands. Subset of controls.
+commands = []
+
+#Gravity boolean. True is Mario, False is Pokemon
+gravity = False
+
+#The player (a sprite)
+player = Player(screen, 400, 300)
+
 #game loop
 while True:
     time_passed = clock.tick(FPS)
@@ -25,7 +37,17 @@ while True:
         elif event.type == KEYDOWN:
             if event.key == K_ESCAPE:
                 quit()
+            elif event.key in controls:
+                commands.append(event.key)
+            elif event.key == K_SPACE:
+                gravity = not gravity
+        elif event.type == KEYUP:
+            if event.key in commands:
+                commands.remove(event.key)
 
     screen.fill((255, 255, 255))
+
+    player.update(commands, gravity)
+    player.draw()
 
     pygame.display.flip() 
