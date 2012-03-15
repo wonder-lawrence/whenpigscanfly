0ad28a611097c5071cf4b1f9d253a54dbc8abd2e
Max Goldstein <maxgoldstein1@gmail.com>
2012-03-15 00:00:37 -0400

    loadLevel closes file

--- loadLevel.py
+++ loadLevel.py
@@ -6,12 +6,12 @@ from Pig import Pig
 
 def loadLevel(level_name, screen):
     """
-    loadLevel - expects the name of a spriteMap and the screen, and returns the
+    loadLevel - expects the name of a sprite map and the screen, and returns the
     sprites for the level.
     
-    The sprite map should consist of spaces, newlines, P's, and exactly one T.
-    Max line length is 40; max number of lines is 30.
-    For best results, align blocks and keep pigs away from edges.
+    The sprite map is the name of a file, whose contents consist of spaces,
+    newlines, P's, and exactly one T.  Max line length is 40; max number of
+    lines is 30. For best results, align blocks and keep pigs away from edges.
     
     Return type: a tuple of one Player object, a possibly empty list of Pig
     objects, and a possibly empty list of Block objects.
@@ -48,4 +48,5 @@ def loadLevel(level_name, screen):
         col += 1
         region = spriteMap.read(1)
 
+    spriteMap.close()
     return player, pigs, blocks
