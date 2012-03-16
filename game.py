f101c8adec8e3d25418f189ee9bd284fd0785dda
Max Goldstein <maxgoldstein1@gmail.com>
2012-03-16 15:37:20 -0400

    Sidescrolling support.

--- game.py
+++ game.py
@@ -7,25 +7,35 @@ def quit():
     sys.exit()
 
 pygame.init()
-screenDimensions = (800, 600)
-window = pygame.display.set_mode(screenDimensions)
+
+#Screen
+WIDTH, HEIGHT = 800, 600
+LEVEL_WIDTH = 2000
+window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('When Pigs Fly')
 screen = pygame.display.get_surface() 
+level = pygame.Surface((LEVEL_WIDTH, HEIGHT))
 
+#Offset between model (level) and view (screen) 
+offset = 0
+
+font = pygame.font.Font(None, 48)
+
+#Clock
 clock = pygame.time.Clock()
 FPS = 50
 time_passed = 0
 
-#static list of controls
+#Static list of controls
 controls = [K_UP, K_DOWN, K_LEFT, K_RIGHT, K_w, K_a, K_s, K_d]
-#dynamic list of active commands. Subset of controls.
+#Dynamic list of active commands. Subset of controls.
 commands = []
 
 #Gravity boolean. True is Mario, False is Pokemon
 gravity = False
 
 #Read in a file to generate the sprites on a level
-player, pigs, blocks = loadLevel("one.txt", screen)
+player, pigs, blocks = loadLevel("one.txt", level)
 #game loop
 while True:
     time_passed = clock.tick(FPS)
@@ -64,9 +74,9 @@ while True:
     #Draw
     #Background
     if gravity:
-        screen.fill((223, 223, 255))
+        level.fill((223, 223, 255))
     else:
-        screen.fill((223, 255, 223))
+        level.fill((223, 255, 223))
 
     #Non-moving sprites (aka "blocks")
     for block in blocks:
@@ -77,4 +87,11 @@ while True:
     for pig in pigs:
         pig.draw()
 
+    if player.x - offset < 50 and offset != 0:
+        offset -= 10
+    if player.x - offset > WIDTH-50 and offset != LEVEL_WIDTH:
+        offset += 10
+
+    screen.blit(level, (-offset, 0))
+    #screen.blit(font.render(str(offset) + " " + str(player.x), 1, (255, 255, 255)), (10,10))
     pygame.display.flip() 
